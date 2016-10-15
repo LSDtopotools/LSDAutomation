c80961e95473cdc980088000a50770f4adeedb07
Simon M. Mudd <simon.m.mudd@ed.ac.uk>
2016-10-15 10:04:12 +0100

    Adding functions for cloning and making different LSDTopoTools packages

--- LSDTT_FileTools/LSDDirectoryManager.py
+++ LSDTT_FileTools/LSDDirectoryManager.py
@@ -31,7 +31,8 @@ def BuildDirectoryTree():
         print path
     else:
         print "Path: " +path+" already exists."         
-        
+
+# This function clones both the test data and the workshop data from github
 def CloneData():
     git = "git"
     clone = "clone"
@@ -55,9 +56,51 @@ def CloneData():
         git_worktree = "--work-tree="+this_cwd+"/LSDTopoTools/Topographic_projects/Test_data/"
         git_dir = "--git-dir="+this_cwd+"/LSDTopoTools/Topographic_projects/Test_data/.git"
         subprocess.call([git,git_worktree,git_dir,pull,origin,master])
+    
+    print "I am going to check the workshop data files."
+    file = "./LSDTopoTools/Topographic_projects/LSDTT_workshop_data/WA.bil"
+    if not os.path.isfile(file):
+        print "There is no test data. Cloning in a subprocess"
+        repo_address = "https://github.com/LSDtopotools/LSDTT_workshop_data.git"
+        target_directory = "./LSDTopoTools/Topographic_projects/LSDTT_workshop_data"
+        subprocess.call([git,clone,repo_address,target_directory])
+    else:
+        print "The repo with " + file+ " exists. I am updating."
+        print "I wonder where I am now"
+        this_cwd = os.getcwd()
+        print "I am at: " + this_cwd
         
+        git_worktree = "--work-tree="+this_cwd+"/LSDTopoTools/Topographic_projects/LSDTT_workshop_data/"
+        git_dir = "--git-dir="+this_cwd+"/LSDTopoTools/Topographic_projects/LSDTT_workshop_data/.git"
+        subprocess.call([git,git_worktree,git_dir,pull,origin,master])   
     
+# This function clones both the test data and the workshop data from github
+def CloneMakeAnalysisDriver():
+    git = "git"
+    clone = "clone"
+    pull = "pull"
+    origin = "origin"
+    master = "master"
     
-    
-    
-    
+    print "I am going to check if the repository exists."
+    file = "./LSDTopoTools/Git_projects/LSDTopoTools_AnalysisDriver/LSDRaster.cpp"
+    if not os.path.isfile(file):
+        print "I don't see the LSDraster.cpp. I am going to try cloning the LSDTopoTools_AnalysisDriver repo."
+        repo_address = "https://github.com/LSDtopotools/LSDTopoTools_AnalysisDriver.git"
+        target_directory = "./LSDTopoTools/Git_projects/LSDTopoTools_AnalysisDriver"
+        subprocess.call([git,clone,repo_address,target_directory])
+    else:
+        print "The repo with " + file+ " exists. I am updating."
+        print "I wonder where I am now"
+        this_cwd = os.getcwd()
+        print "I am at: " + this_cwd
+        
+        git_worktree = "--work-tree="+this_cwd+"/LSDTopoTools/Git_projects/LSDTopoTools_AnalysisDriver/"
+        git_dir = "--git-dir="+this_cwd+"/LSDTopoTools/Git_projects/LSDTopoTools_AnalysisDriver/.git"
+        subprocess.call([git,git_worktree,git_dir,pull,origin,master])    
+        
+    print "I should have the AnalysisDriver. Now I am going to make the program for you."
+    makef = "make -f"
+    LSDTTpath = "/LSDTopoTools/Git_projects/"
+    target = this_cwd+LSDTTpath+"/LSDTopoTools_AnalysisDriver/Analysis_driver/Drive_analysis_from_paramfile.make"
+    subprocess.call([makef,target])  
