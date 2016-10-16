ebcd2606c768398830a43c506a8d0c1420e6e5bb
Simon M. Mudd <simon.m.mudd@ed.ac.uk>
2016-10-16 09:40:48 +0100

    Some changes to the setup prgarm

--- LSDTopoToolsSetup.py
+++ LSDTopoToolsSetup.py
@@ -6,7 +6,29 @@
 
 import os
 
-def LSDTopoToolsSetUp():
+def LSDTopoToolsSetUp(WantHomeDirectory = True):
     
     # First, we need to find out if we are on vagrant
     home_dir = os.path.expanduser("~")
+    this_dir = os.path.getcwd()
+    
+    print "You home directory is: "+home_dir
+    
+    vagrant_str = "vagrant"
+    if vagrant_str in home_dir:
+        print "You seem to be on a vagrant machine"
+        vagrant_switch = True
+    else:
+        print "You don't seem to be using vagrant."
+        if WantHomeDirectory == True:
+            print "I am going to install on your home directory, which is here:"
+            print home_dir
+        else:
+            print "You set the WantHomeDirectory to false, so I am going to install here:"
+            print this_dir
+        
+        
+    
+    
+if __name__ == "__main__":
+    LSDTopoToolsSetUp() 
