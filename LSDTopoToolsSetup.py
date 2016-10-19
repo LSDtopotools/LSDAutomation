cacd89077b91bdd6b71ae0521f3d7e3916134660
Simon M. Mudd <simon.m.mudd@ed.ac.uk>
2016-10-19 12:21:34 +0100

    Working on parsing arguments to the setup program

--- LSDTopoToolsSetup.py
+++ LSDTopoToolsSetup.py
@@ -6,6 +6,8 @@
 
 import os
 import subprocess
+import sys, getopt
+import time
 
 #=============================================================================
 # This function builds the directory trees
@@ -45,8 +47,7 @@ def BuildDirectoryTree(the_base_directory):
 #=============================================================================
         
         
-        
-        
+               
 #=============================================================================
 # This function clones both the test data and the workshop data from github
 #=============================================================================
@@ -141,13 +142,15 @@ def CloneMakeAnalysisDriver(the_base_directory):
 #=============================================================================
 # This function clones both the test data and the workshop data from github
 #=============================================================================
-def CloneMakeChiMudd(the_base_directory):
+def CloneMakeChiTools(the_base_directory):
     git = "git"
     clone = "clone"
     pull = "pull"
     origin = "origin"
     master = "master"
-    
+ 
+    # The below logic checks to see if the repo exist. If not it clones, if so it pulls, both using a 
+    # subprocess call to git
     print "I am going to check if the repository exists."
     file = the_base_directory+"LSDTopoTools/Git_projects/LSDTopoTools_ChiMudd2014/LSDRaster.cpp"
     if not os.path.isfile(file):
@@ -185,6 +188,67 @@ def CloneMakeChiMudd(the_base_directory):
     print "Note if make said it didn't have anything to do it means you already compiled the program."
 #=============================================================================   
 
+
+
+#=============================================================================
+# This function clones both the test data and the workshop data from github
+#=============================================================================
+def CloneMakeChiMudd(the_base_directory):
+    git = "git"
+    clone = "clone"
+    pull = "pull"
+    origin = "origin"
+    master = "master"
+    
+    # The below logic checks to see if the repo exist. If not it clones, if so it pulls, both using a 
+    # subprocess call to git
+    print "I am going to check if the repository exists."
+    file = the_base_directory+"LSDTopoTools/Git_projects/LSDTopoTools_ChiMudd2014/LSDRaster.cpp"
+    if not os.path.isfile(file):
+        print "I don't see the LSDraster.cpp. I am going to try cloning the LSDTopoTools_ChiMudd2014 repo."
+        repo_address = "https://github.com/LSDtopotools/LSDTopoTools_ChiMudd2014.git"
+        target_directory = the_base_directory+"LSDTopoTools/Git_projects/LSDTopoTools_ChiMudd2014"
+        subprocess.call([git,clone,repo_address,target_directory])
+    else:
+        print "The repo with " + file+ " exists. I am updating."
+        git_worktree = "--work-tree="+the_base_directory+"LSDTopoTools/Git_projects/LSDTopoTools_ChiMudd2014/"
+        git_dir = "--git-dir="+the_base_directory+"/LSDTopoTools/Git_projects/LSDTopoTools_ChiMudd2014/.git"
+        subprocess.call([git,git_worktree,git_dir,pull,origin,master])    
+        
+    print "I've got the repository. Now I am going to make the program for you."
+    make = "make"
+    C_flag = "-C"
+    LSDTTpath = "LSDTopoTools/Git_projects/"
+    f_flag = "-f"
+    target_path = the_base_directory+LSDTTpath+"LSDTopoTools_ChiMudd2014/driver_functions_MuddChi2014/"
+    
+    # Get the list of makefiles
+    makefile_list = []
+    makefile_list.append("chi_step1_write_junctions.make")
+    makefile_list.append("chi_step2_write_channel_file.make")
+    makefile_list.append("chi_get_profiles.make")
+    makefile_list.append("chi_m_over_n_analysis.make")
+    
+    # Loop through the makefile list, calling make as you go using a subprocess
+    for target_makefile in makefile_list:
+        
+        print "I am making using the makefile: "+target_makefile
+        target_makefile = "chi_mapping_tool.make"
+    
+        target = target_path+target_makefile
+        
+        # Check to see if the makefile is here
+        if not os.path.isfile(target):
+            print "The makefile doesn't exist. Check your filenames and paths."
+        else:
+            print "Makefile is here, lets run make!"
+        
+        # Call make via subprocess
+        subprocess.call([make,C_flag,target_path,f_flag,target_makefile])
+    
+    print "I've compiled everything you need to run the Mudd et al 2014 JGR-ES analyses!"
+    print "Note if make said it didn't have anything to do it means you already compiled the program."       
+#=============================================================================   
 
 #=============================================================================
 # This is the main function that drives all cloning and directory creation
@@ -254,8 +318,57 @@ def LSDTopoToolsSetUp(WantHomeDirectory = True):
     print "\n\nNow I'll get the analysis driver and compile it."
     CloneMakeAnalysisDriver(the_base_directory)
 #=============================================================================
-        
+
+#=============================================================================
+# This is the main function that runs the whole thing 
+#=============================================================================
+def main(argv):
+ 
+    # If there are no arguments, send to the welcome screen
+    if not len(sys.argv) > 1:
+        print_welcome()
+        sys.exit()
+
+    # Get the arguments
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-id", "--installation_directory",type=int, default=0, choices=[0, 1],                        help="Tells the program where to install LSDTopoTools.\nOptions are: 0 == home directory, 1 == current directory.\nIf you are in vagrant it will ignore this option and install in the root directory.")
+    args = parser.parse_args()
+
+    
+    if args.installation_directory == 0:
+        LSDTopoToolsSetUp(True)
+    elif args.installation_directory == 1:
+        LSDTopoToolsSetUp(False)  
+    
+    #LSDTopoToolsSetUp() 
+#=============================================================================
     
     
+#=============================================================================    
+# This is just a welcome screen that is displayed if no arguments are provided.
+#=============================================================================
+def print_welcome():
+
+    print "\n\n======================================================================="
+    print "Hello there, I am the going to help you set up LSDTopoTools!"
+    print "You will need to tell me what to do."
+    print "If you are in vagrant, LSDTopoTools will always be installed in the root directory (\)."
+    print "If you are not in vagrant,"
+    print "LSDTopoTools will be installed in your home directory if you run this with:"
+    print "   python LSDTopoToolsSetup.py -id 0\n"
+    print "Or your current directory."
+    print "(i.e., the directory from which you called this program)\n"
+    print "if you run with this:"
+    print "   python LSDTopoToolsSetup.py -id 1\n"
+    print "For help type:"
+    print "   python LSDTopoToolsSetup.py -h\n"
+    print "=======================================================================\n\n "
+#=============================================================================
+
+
+
 if __name__ == "__main__":
-    LSDTopoToolsSetUp() 
+    main(sys.argv[1:])
+    
+    
